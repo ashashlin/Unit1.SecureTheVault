a6601453e474df5881aa76c4be3dc82804b4743a
Ashley Lin <linashleyy@gmail.com>
2025-04-18 04:04:28 +0800

    Finish JS

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+const code2 = (100 - 20) / 2;
+
+const code3 = (6 % 2) + 39;
+
+const message = "The vault has been secured. The combination is:";
+
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
